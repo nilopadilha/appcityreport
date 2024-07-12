3d99b5e42c70ceab5b4f674a682578f1a65bdf11
nilopadilha <nilopadilha@gmail.com>
2024-07-12 20:07:13 -0300

    primeiro commit da aplicação de manipulação de dados estratégicos

---
+++ src/main/java/br/com/solivos/appcityreport/AppcityreportApplication.java
@@ -0,0 +1,13 @@
+package br.com.solivos.appcityreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppcityreportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppcityreportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/solivos/appcityreport/AppcityreportApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.solivos.appcityreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppcityreportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
